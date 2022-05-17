e876a0c79454286167312cc0146d2a6b5a6bb763
Gerald Maduabuchi <mgeraldoj07@gmail.com>
2022-05-17 09:16:22 +0100

    Include Queue Algorithm

---
+++ GreedyAlgorithms/Queue/Queue.py
@@ -0,0 +1,27 @@
+# Implementing a Queue
+  
+# Import doubly ended queue module.
+from collections import deque
+  
+# Initialize queue.
+q = deque()
+  
+# Include values in Queue
+q.append('first value')
+q.append('second value')
+q.append('third value')
+
+print("first queue")
+
+print(q)
+  
+# Remove values in a queue
+print("\nValues removed from the queue")
+
+print(q.popleft())
+print(q.popleft())
+print(q.popleft())
+  
+print("\nCurrent Values after removal")
+print(q)
+  
